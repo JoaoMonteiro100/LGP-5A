412113ea08029e4d5fb967f50930f43f543cf7e1
João Monteiro <joaomiguelmonteiro@gmail.com>
2016-05-04 01:50:22 +0100

    Added 6 tests, 3 functions & a few other things
    Including a new enum to see in what units each result is

--- Analysis/src/Calculations.java
+++ Analysis/src/Calculations.java
@@ -3,97 +3,132 @@ import java.util.HashMap;
 /**
  * Created by joaom on 26/04/2016.
  */
-public class Calculations {
+class Calculations {
+    private static HashMap<TypesOfCalculations, String[]> units;
+    static
+    {
+        units.put(TypesOfCalculations.WAVELENGTH, new String[] {"Meter", "m"});
+        units.put(TypesOfCalculations.WAVENUMBER, new String[] {"Cycles per Meter", "c/m"});
+        units.put(TypesOfCalculations.AMPLITUDE, new String[] {"Volts per Meter", "V/m"});
+        units.put(TypesOfCalculations.PERIOD, new String[] {"Second", "s"});
+        units.put(TypesOfCalculations.VELOCITY, new String[] {"Meter per Second", "m/s"});
+        units.put(TypesOfCalculations.MAX, new String[] {"Hertz", "Hz"});
+        units.put(TypesOfCalculations.MIN, new String[] {"Hertz", "Hz"});
+        units.put(TypesOfCalculations.MEAN, new String[] {"Hertz", "Hz"});
+    }
+
+    private float[][] values;
+    private TypesOfCalculations[] calcs;
+    private float frequencyMean;
+    private HashMap<TypesOfCalculations, Float> result;
+    private final float brainwaveSpeed = 25; //in m/s and assuming the subject is thinking. SOURCE: http://hypertextbook.com/facts/2002/DavidParizh.shtml
+
+    Calculations(float[][] values, TypesOfCalculations[] calcs) {
+        this.values = values;
+        this.calcs = calcs;
+        this.result = new HashMap<TypesOfCalculations, Float>();
+
+        //in case any of the values is null, result is null as well
+        for(float[] iter : values) {
+            if (iter.length == 0) {
+                this.result = null;
+                return;
+            }
+        }
+
+        frequencyMean();
+        calc();
+    }
+
+    HashMap<TypesOfCalculations, Float> getResult() {
+        return this.result;
+    }
+
+    public HashMap<TypesOfCalculations, String[]> getUnits() {
+        return units;
+    }
 
     //see what kind of calculations are needed and for each one call the appropriate functions
-    public HashMap<TypesOfCalculations, Double> Calculations(double[][] values, TypesOfCalculations[] calcs) {
+    private void calc() {
 
-        //hashmap of types of calcs and their respective results
-        HashMap<TypesOfCalculations, Double> result = new HashMap<>();
-
-        for(TypesOfCalculations type : calcs) {
+        for(TypesOfCalculations type : this.calcs) {
             switch(type) {
                 case WAVELENGTH:
-                    result.put(TypesOfCalculations.WAVELENGTH, calcWaveLength(values));
+                    this.result.put(TypesOfCalculations.WAVELENGTH, calcWaveLength(values));
 
                 case WAVENUMBER:
-                    result.put(TypesOfCalculations.WAVENUMBER, calcWaveNumber(values));
+                    this.result.put(TypesOfCalculations.WAVENUMBER, calcWaveNumber(values));
 
                 case AMPLITUDE:
-                    result.put(TypesOfCalculations.AMPLITUDE, calcAmplitude(values));
-
-                case FREQUENCY:
-                    result.put(TypesOfCalculations.FREQUENCY, calcFrequency(values));
+                    this.result.put(TypesOfCalculations.AMPLITUDE, calcAmplitude(values));
 
                 case PERIOD:
-                    result.put(TypesOfCalculations.PERIOD, calcPeriod(values));
+                    this.result.put(TypesOfCalculations.PERIOD, calcPeriod(values));
 
                 case VELOCITY:
-                    result.put(TypesOfCalculations.VELOCITY, calcVelocity(values));
+                    this.result.put(TypesOfCalculations.VELOCITY, calcVelocity(values));
 
                 case MAX:
-                    result.put(TypesOfCalculations.MAX, calcMax(values));
+                    this.result.put(TypesOfCalculations.MAX, calcMax(values));
 
                 case MIN:
-                    result.put(TypesOfCalculations.MIN, calcMin(values));
+                    this.result.put(TypesOfCalculations.MIN, calcMin(values));
 
                 case MEAN:
-                    result.put(TypesOfCalculations.MEAN, calcMean(values));
+                    this.result.put(TypesOfCalculations.MEAN, calcMean(values));
 
                 default:
                     break;
             }
         }
+    }
 
-        if(result.isEmpty()) {
-            return null;
+    private void frequencyMean() {
+        float total = 0;
+        int i = 0;
+
+        for(float[] valueTuple : values) {
+            total += valueTuple[1];
+            i++;
         }
 
-        return result;
+        this.frequencyMean = total / i;
     }
 
-    private double calcWaveLength(double[][] values) {
+    private float calcWaveLength(float[][] values) {
+        return brainwaveSpeed / this.frequencyMean;
+    }
+
+    private float calcWaveNumber(float[][] values) {
+        return this.frequencyMean / brainwaveSpeed;
+    }
+
+    private float calcAmplitude(float[][] values) {
         //TODO
         return 0;
     }
 
-    private double calcWaveNumber(double[][] values) {
+    private float calcPeriod(float[][] values) {
         //TODO
         return 0;
     }
 
-    private double calcAmplitude(double[][] values) {
+    private float calcVelocity(float[][] values) {
         //TODO
         return 0;
     }
 
-    private double calcFrequency(double[][] values) {
+    private float calcMax(float[][] values) {
         //TODO
         return 0;
     }
 
-    private double calcPeriod(double[][] values) {
+    private float calcMin(float[][] values) {
         //TODO
         return 0;
     }
 
-    private double calcVelocity(double[][] values) {
-        //TODO
-        return 0;
-    }
-
-    private double calcMax(double[][] values) {
-        //TODO
-        return 0;
-    }
-
-    private double calcMin(double[][] values) {
-        //TODO
-        return 0;
-    }
-
-    private double calcMean(double[][] values) {
-        //TODO
-        return 0;
+    private float calcMean(float[][] values) {
+        return this.frequencyMean;
     }
 }
--- Analysis/src/Tests.java
+++ Analysis/src/Tests.java
@@ -1,48 +1,79 @@
 import org.junit.Test;
 
+import static junit.framework.TestCase.assertNotNull;
+import static junit.framework.TestCase.assertNull;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
 public class Tests {
 
-    final double[][][] values = {
+    final float[][][] values = {
             //always the same value
-            {{0.0, 20}, {0.1, 20}, {0.2, 20}, {0.3, 20}, {0.4, 20}, {0.5, 20}},
+            {{0.0f, 20f}, {0.1f, 20f}, {0.2f, 20f}, {0.3f, 20f}, {0.4f, 20f}, {0.5f, 20f}},
             //different values
-            {{0.01, 0}, {0.02, 0}, {0.03, 100}, {0.04, 100}},
+            {{0.01f, 0f}, {0.02f, 0f}, {0.03f, 100f}, {0.04f, 100f}},
             //different values with decimal result
-            {{0.01, 1}, {0.02, 1}, {0.03, 2}, {0.04, 2}},
+            {{0.01f, 1f}, {0.02f, 1f}, {0.03f, 2f}, {0.04f, 2f}},
             //results not ordered by time
-            {{0.03, 50}, {0.04, 0}, {0.01, 65}, {0.02, 30}},
+            {{0.03f, 50f}, {0.04f, 0f}, {0.01f, 65f}, {0.02f, 30f}},
             //1 value only
-            {{0, 50}},
+            {{0f, 50f}},
             //result with 3 decimal units
-            {{0.01, 0.25}, {0.02, 0.26}},
+            {{0.01f, 0.25f}, {0.02f, 0.26f}},
             //same values at the same time
-            {{0.00, 50}, {0.00, 50}},
+            {{0.00f, 50f}, {0.00f, 50f}},
             //different values at the same time
-            {{0.00, 50}, {0.00, 100}},
+            {{0.00f, 50f}, {0.00f, 100f}},
+            //no value
+            {{}},
+    };
+
+    final TypesOfCalculations[][] types = {
+            {TypesOfCalculations.WAVELENGTH},
+            {TypesOfCalculations.WAVENUMBER},
+            {TypesOfCalculations.AMPLITUDE},
+            {TypesOfCalculations.PERIOD},
+            {TypesOfCalculations.VELOCITY},
+            {TypesOfCalculations.MAX},
+            {TypesOfCalculations.MIN},
+            {TypesOfCalculations.MEAN},
+            {TypesOfCalculations.MAX,TypesOfCalculations.MIN,TypesOfCalculations.MEAN},
     };
 
     @Test
     public void testMain() {
-        //TODO
+        Calculations calcs = new Calculations(values[1],types[0]);
+        assertTrue(calcs.getResult().containsKey(TypesOfCalculations.WAVELENGTH));
+        assertNotNull(calcs.getResult().values());
+
+        Calculations calcs2 = new Calculations(values[3],types[4]);
+        assertTrue(calcs2.getResult().containsKey(TypesOfCalculations.VELOCITY));
+        assertNotNull(calcs2.getResult().values());
+
+        Calculations calcs3 = new Calculations(values[8],types[8]);
+        assertNull(calcs3.getResult());
     }
 
     @Test
     public void testWaveLength() {
-        //TODO
+        Calculations calcs = new Calculations(values[0],types[0]);
+        assertEquals(1.25,calcs.getResult().get(TypesOfCalculations.WAVELENGTH),0.001);
+
+        Calculations calcs2 = new Calculations(values[1],types[0]);
+        assertEquals(0.5,calcs2.getResult().get(TypesOfCalculations.WAVELENGTH),0.1);
     }
 
     @Test
     public void testWaveNumber() {
-        //TODO
+        Calculations calcs = new Calculations(values[0],types[1]);
+        assertEquals(0.8,calcs.getResult().get(TypesOfCalculations.WAVENUMBER),0.01);
+
+        Calculations calcs2 = new Calculations(values[1],types[1]);
+        assertEquals(2,calcs2.getResult().get(TypesOfCalculations.WAVENUMBER),0);
     }
 
     @Test
     public void testAmplitude() {
-        //TODO
-    }
-
-    @Test
-    public void testFrequency() {
         //TODO
     }
 
@@ -58,16 +89,37 @@ public class Tests {
 
     @Test
     public void testMax() {
-        //TODO
+        Calculations calcs = new Calculations(values[0],types[5]);
+        assertEquals(20,calcs.getResult().get(TypesOfCalculations.MAX),0);
+
+        Calculations calcs2 = new Calculations(values[2],types[5]);
+        assertEquals(65,calcs2.getResult().get(TypesOfCalculations.MAX),0);
+
+        Calculations calcs3 = new Calculations(values[8],types[5]);
+        assertNull(calcs3.getResult());
     }
 
     @Test
     public void testMin() {
-        //TODO
+        Calculations calcs = new Calculations(values[4],types[6]);
+        assertEquals(50,calcs.getResult().get(TypesOfCalculations.MIN),0);
+
+        Calculations calcs2 = new Calculations(values[6],types[6]);
+        assertEquals(50,calcs2.getResult().get(TypesOfCalculations.MIN),0);
+
+        Calculations calcs3 = new Calculations(values[7],types[6]);
+        assertEquals(50,calcs3.getResult().get(TypesOfCalculations.MIN),0);
     }
 
     @Test
     public void testMean() {
-        //TODO
+        Calculations calcs = new Calculations(values[0],types[7]);
+        assertEquals(20,calcs.getResult().get(TypesOfCalculations.MEAN),0);
+
+        Calculations calcs2 = new Calculations(values[5],types[7]);
+        assertEquals(0.255,calcs2.getResult().get(TypesOfCalculations.MEAN),0.0001);
+
+        Calculations calcs3 = new Calculations(values[8],types[7]);
+        assertNull(calcs3.getResult());
     }
 }
