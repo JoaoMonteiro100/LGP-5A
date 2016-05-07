dd51e917209085b227d7adf290cfc4917932382a
André Pinheiro <andrepdpinheiro@gmail.com>
2016-05-07 18:42:39 +0100

    Started to code the UI

---
+++ FW/src/com/lgp5/fw/controllers/MenuController.java
@@ -0,0 +1,7 @@
+package com.lgp5.fw.controllers;
+
+/**
+ * Created by pluralism on 07-05-2016.
+ */
+public class MenuController {
+}
---
+++ FW/src/com/lgp5/fw/controllers/SelectDeviceController.java
@@ -0,0 +1,65 @@
+package com.lgp5.fw.controllers;
+
+
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+
+public class SelectDeviceController {
+    @FXML
+    private ComboBox selectDeviceComboBox;
+    @FXML
+    private Button selectDeviceStartButton;
+
+
+
+    public SelectDeviceController() {
+    }
+
+
+    // Called after the FXML has been initialized
+    @FXML
+    private void initialize() {
+        selectDeviceComboBox.getItems().addAll("NeuroSky Mindset", "Emotiv Epoc");
+
+
+        selectDeviceStartButton.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                String comboxBoxValue = selectDeviceComboBox.getSelectionModel().getSelectedItem().toString();
+                if(comboxBoxValue != null) {
+                    switch (comboxBoxValue) {
+                        case "NeuroSky Mindset":
+                            launchNeuroSkyView();
+                            break;
+
+                        default:
+                            break;
+                    }
+                }
+            }
+        });
+    }
+
+
+    private void launchNeuroSkyView() {
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("../views/menuView.fxml"));
+            Parent parent = (Parent) loader.load();
+            Stage stage = new Stage();
+            stage.setScene(new Scene(parent));
+            stage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
