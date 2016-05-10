49a0f2e3527aeabe27ff0877c47db9a30a9f5abe
siualpinto <lsmoreira_77@hotmail.com>
2016-05-10 20:26:37 +0100

    remove teste metod

--- FW/src/com/lgp5/fw/Main.java
+++ FW/src/com/lgp5/fw/Main.java
@@ -1,14 +1,11 @@
 package com.lgp5.fw;
 
 import javafx.application.Application;
-import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.text.Font;
 import javafx.stage.Stage;
-import javafx.event.ActionEvent;
-
 import java.io.File;
 import java.io.FileInputStream;
 
@@ -30,9 +27,6 @@ public class Main extends Application {
 
     public static void main(String[] args) {
         launch(args);
-    }
+    }   
    
-    public void teste(){
-    	System.out.println("teste");
-    }
 }
