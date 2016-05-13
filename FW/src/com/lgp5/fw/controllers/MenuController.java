afb133766fa7c7087e67dad53a6af8facdaeab7a
siualpinto <lsmoreira_77@hotmail.com>
2016-05-13 17:22:31 +0100

    add bars on barChart

--- FW/src/com/lgp5/fw/controllers/MenuController.java
+++ FW/src/com/lgp5/fw/controllers/MenuController.java
@@ -6,7 +6,15 @@ import com.lgp5.api.neurosky.Neurosky_FW.utils.Constants;
 
 import javafx.animation.FadeTransition;
 import javafx.application.Platform;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.scene.Node;
+import javafx.scene.chart.BarChart;
+import javafx.scene.chart.CategoryAxis;
+import javafx.scene.chart.XYChart;
 import javafx.scene.control.*;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
@@ -15,17 +23,20 @@ import javafx.scene.layout.GridPane;
 import javafx.scene.layout.Pane;
 import javafx.util.Duration;
 
+import java.util.Arrays;
 import java.util.HashMap;
 
 
 public class MenuController {
-
+	private int colorNumber=0;
 	@FXML private AnchorPane paneLayoutRoot;
 	@FXML private ImageView arrowLabel;
 	@FXML private Pane painelHSA;
 	@FXML private Label historyLabel;
 	@FXML private Label sensorsLabel;
 	@FXML private Label analysisLabel;
+	@FXML private Label brainWavesLabel;
+	@FXML private Label dataLabel;
 	@FXML private GridPane brainWavesPane;
 	@FXML private Label gamma1Data;
 	@FXML private Label gamma2Data;
@@ -42,14 +53,29 @@ public class MenuController {
 	@FXML private Label signalQualityData;
 	@FXML private GridPane dataPane;
 	private HeadSetDataInterface headSetDataInterface;
+	@FXML private BarChart<String, Float> barChart;
+	@FXML private CategoryAxis xAxis;
+	private ObservableList<String> brainwaves = FXCollections.observableArrayList();
 
 
 	public MenuController() {
 	}
 
-
+	/**
+	 * Initializes the controller class and sets x axis of the bar chart with the appropriate values
+	 */
 	@FXML
 	private void initialize() {
+		String[] waves = {"Delta", "Theta", "Alfa 1", "Alfa 2", "Beta 1", "Beta 2", "Gamma 1", "Gamma 2"};
+		brainwaves.addAll(Arrays.asList(waves));
+		xAxis.setCategories(brainwaves);
+
+
+		float[] values = {35f, 43f, 27f, 12f, 9.2f, 32f, 16f, 20f};
+		XYChart.Series<String,Float> series = createWaveDataSeries(values);
+		barChart.getData().add(series);
+		barChart.setLegendVisible(false);
+
 		headSetDataInterface = new HeadSetDataInterface() {
 			@Override
 			public void onReceiveData(HashMap<String, HashMap<String, Object>> hashMap) {
@@ -85,7 +111,7 @@ public class MenuController {
 				}
 			}
 		};
-		
+
 		new Thread(new Neurosky("0013EF004809", headSetDataInterface)).start();
 	}
 
@@ -110,6 +136,8 @@ public class MenuController {
 
 
 	public void showData(MouseEvent event){
+		dataLabel.setDisable(true);
+		brainWavesLabel.setDisable(false);
 		try {
 			if(!dataPane.isVisible()) {
 				brainWavesPane.setVisible(false);
@@ -127,6 +155,8 @@ public class MenuController {
 
 
 	public void showBrainWaves(MouseEvent event) {
+		dataLabel.setDisable(false);
+		brainWavesLabel.setDisable(true);
 		try {
 			if(!brainWavesPane.isVisible()) {
 				dataPane.setVisible(false);
@@ -135,5 +165,38 @@ public class MenuController {
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
+	}
+
+
+	/**
+	 * Creates a XYChart.Data object for each wave. All data is then returned as a series.
+	 *
+	 * @param values Array with a value for each brainwave. Must be the same length as the waves array
+	 * @return Series of brainwave data
+	 */
+	private XYChart.Series<String,Float> createWaveDataSeries (float[] values) {
+
+		XYChart.Series<String,Float> series = new XYChart.Series<>();
+
+		for (int i = 0; i < values.length; i++) {
+			XYChart.Data<String,Float> waveData = new XYChart.Data<>(brainwaves.get(i), values[i]);
+			waveData.nodeProperty().addListener(new ChangeListener<Node>() {
+				
+				@Override public void changed(ObservableValue<? extends Node> ov, Node oldNode, Node newNode) {					
+					if (newNode != null) { 
+						if(colorNumber>constants.Constants.colors.length)
+						{
+							colorNumber=0;
+						}
+						newNode.setStyle("-fx-bar-fill: "+constants.Constants.colors[colorNumber]+";"); 	
+						colorNumber++;						
+					}					
+				}
+				
+			});
+			series.getData().add(waveData);         
+		}
+
+		return series;
 	}
 }
