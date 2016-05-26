069680c8a795c7c913fbf053442a3742c64a5f50
PsyGwendok <gwendok@hotmail.com>
2016-05-26 19:36:51 +0100

    Same names for contasts as in Emotiv

--- Neurosky_FW/src/utils/Constants.java
+++ Neurosky_FW/src/utils/Constants.java
@@ -1,18 +1,6 @@
 package utils;
 
 public class Constants {
-<<<<<<< HEAD
-	public static final String DELTA = "delta";
-	public static final String THETA = "theta";	
-	public static final String LOW_ALPHA = "low_alpha";
-	public static final String HIGH_ALPHA = "high_alpha";
-	public static final String LOW_BETA = "low_beta";
-	public static final String HIGH_BETA = "high_beta";
-	public static final String LOW_GAMMA = "low_gamma";
-	public static final String MID_GAMMA = "mid_gamma";
-	public static final String ATTENTION = "attention";
-	public static final String MEDITATION = "meditation";
-=======
 	public static final String DELTA = "Delta";
 	public static final String THETA = "Theta";	
 	public static final String LOW_ALPHA = "LowAlpha";
@@ -24,14 +12,9 @@ public class Constants {
 	public static final String ATTENTION = "Attention";
 	public static final String MEDITATION = "Meditation";
 	public static final String BLINK = "Blink";
->>>>>>> refs/remotes/origin/issue18
 	public static final String BATTERY_LEVEL = "battery_level";
 	public static final String POOR_SIGNAL = "poor_signal";
 	
 	
 	public static final String WAVES = "Waves";
-<<<<<<< HEAD
 }
-=======
-}
->>>>>>> refs/remotes/origin/issue18
