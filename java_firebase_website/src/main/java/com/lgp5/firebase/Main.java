eb17016fbfcd73969a2efe7bd0d04f328b3cd634
José Lima <jlima91@hotmail.com>
2016-05-26 03:24:57 +0100

    neurosky real time chart

---
+++ java_firebase_website/src/main/java/com/lgp5/firebase/Main.java
@@ -0,0 +1,43 @@
+package com.lgp5.firebase;
+
+
+import java.util.HashMap;
+
+import com.firebase.client.Firebase;
+import com.lgp5.api.neurosky.Neurosky_FW.Neurosky;
+import com.lgp5.api.neurosky.Neurosky_FW.utils.Constants;
+import com.lgp5.api.neurosky.Neurosky_FW.interfaces.HeadSetDataInterface;
+
+
+public class Main {
+    public static void main(String[] args) {
+
+
+
+        Thread t1 = new Thread(new Runnable() {
+            public void run()
+            {
+              final long tStart = System.currentTimeMillis();
+                final Firebase ref = new Firebase("https://brainlight.firebaseio.com/leituras");
+                HeadSetDataInterface headSetDataInterface = new HeadSetDataInterface() {
+
+                    @Override
+                    public void onReceiveData(HashMap<String, HashMap<String, Object>> hashMap) {
+                        HashMap<String, Object> values = hashMap.get(Constants.WAVES);
+                        long tEnd = System.currentTimeMillis();
+                        long tDelta = tEnd - tStart;
+                        double elapsedSeconds = tDelta / 1000.0;
+
+                        values.put("elapsedTime",elapsedSeconds);
+                        ref.child("leitura1").push().setValue(values);
+                    }
+                };
+                new Thread(new Neurosky("0013EF004809", headSetDataInterface)).start();
+
+
+
+            }});
+        t1.start();
+
+    }
+}
