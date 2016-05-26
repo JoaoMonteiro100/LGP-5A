947ff3e3d698ac16624d6d3238e01d9d1dff0ffe
João Monteiro <joaomiguelmonteiro@gmail.com>
2016-05-26 20:18:57 +0100

    Analysis working

--- FW/src/com/lgp5/fw/controllers/AnalysisController.java
+++ FW/src/com/lgp5/fw/controllers/AnalysisController.java
@@ -1,12 +1,16 @@
 package com.lgp5.fw.controllers;
 
 
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
 import javafx.scene.control.ComboBox;
+import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -14,11 +18,15 @@ import java.io.IOException;
 
 public class AnalysisController {
     @FXML
-    private ComboBox waveToAnalyse;
+    private ComboBox<String> waveToAnalyse;
     @FXML
     private Button startAnalysis;
-
-
+    @FXML
+    private TextField analysisPeriodField;
+    @FXML
+    private Button startAnalysisButton;
+    @FXML
+    private CheckBox wavelength, wavenumber, angWavenumber, angFrequency, period, amplitude, median, mode, mean, maxFrequency, minFrequency, maxAmplitude;
 
     public AnalysisController() {
     }
@@ -29,34 +37,148 @@ public class AnalysisController {
     private void initialize() {
         waveToAnalyse.getItems().addAll("Delta", "Theta", "Alfa 1", "Alfa 2", "Beta 1", "Beta 2", "Gamma 1", "Gamma 2");
 
-/*
-        selectDeviceStartButton.setOnAction(new EventHandler<ActionEvent>() {
+        // force the field to be numeric only
+        analysisPeriodField.textProperty().addListener((observable, oldValue, newValue) -> {
+            if (!newValue.matches("\\d*")) {
+                analysisPeriodField.setText(newValue.replaceAll("[^\\d]", ""));
+            }
+        });
+
+        //if the user first clicks on a checkbox, and then selects a wave
+        waveToAnalyse.setOnAction(new EventHandler<ActionEvent>() {
             @Override
             public void handle(ActionEvent event) {
-                Object comboBox = waveToAnalyse.getSelectionModel().getSelectedItem();
-                if(comboBox != null) {
-                    String comboBoxValue = comboBox.toString();
-                    switch (comboBoxValue) {
-                        case "NeuroSky Mindset":
-                            launchNeuroSkyView();
-                            //get a handle to the stage
-                            Stage stage = (Stage) waveToAnalyse.getScene().getWindow();
-                            //close current window
-                            stage.close();
-                            break;
+                if(wavelength.isSelected() ||
+                        wavenumber.isSelected() ||
+                        angWavenumber.isSelected() ||
+                        angFrequency.isSelected() ||
+                        period.isSelected() ||
+                        amplitude.isSelected() ||
+                        median.isSelected() ||
+                        mode.isSelected() ||
+                        mean.isSelected() ||
+                        maxFrequency.isSelected() ||
+                        minFrequency.isSelected() ||
+                        maxAmplitude.isSelected())
+                startAnalysisButton.setDisable(false);
+            }
+        });
 
-                        default:
-                            break;
-                    }
+        //if the user first selects the wave and then the methods
+        wavelength.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                if(waveToAnalyse.getSelectionModel().getSelectedItem()!=null) {
+                    startAnalysisButton.setDisable(false);
                 }
             }
-        });*/
+        });
+        wavenumber.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                if(waveToAnalyse.getSelectionModel().getSelectedItem()!=null) {
+                    startAnalysisButton.setDisable(false);
+                }
+            }
+        });
+        angWavenumber.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                if(waveToAnalyse.getSelectionModel().getSelectedItem()!=null) {
+                    startAnalysisButton.setDisable(false);
+                }
+            }
+        });
+        angFrequency.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                if(waveToAnalyse.getSelectionModel().getSelectedItem()!=null) {
+                    startAnalysisButton.setDisable(false);
+                }
+            }
+        });
+        period.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                if(waveToAnalyse.getSelectionModel().getSelectedItem()!=null) {
+                    startAnalysisButton.setDisable(false);
+                }
+            }
+        });
+        amplitude.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                if(waveToAnalyse.getSelectionModel().getSelectedItem()!=null) {
+                    startAnalysisButton.setDisable(false);
+                }
+            }
+        });
+        mean.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                if(waveToAnalyse.getSelectionModel().getSelectedItem()!=null) {
+                    startAnalysisButton.setDisable(false);
+                }
+            }
+        });
+        mode.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                if(waveToAnalyse.getSelectionModel().getSelectedItem()!=null) {
+                    startAnalysisButton.setDisable(false);
+                }
+            }
+        });
+        median.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                if(waveToAnalyse.getSelectionModel().getSelectedItem()!=null) {
+                    startAnalysisButton.setDisable(false);
+                }
+            }
+        });
+        maxFrequency.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                if(waveToAnalyse.getSelectionModel().getSelectedItem()!=null) {
+                    startAnalysisButton.setDisable(false);
+                }
+            }
+        });
+        minFrequency.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                if(waveToAnalyse.getSelectionModel().getSelectedItem()!=null) {
+                    startAnalysisButton.setDisable(false);
+                }
+            }
+        });
+        maxAmplitude.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                if(waveToAnalyse.getSelectionModel().getSelectedItem()!=null) {
+                    startAnalysisButton.setDisable(false);
+                }
+            }
+        });
+
+
+        startAnalysisButton.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                launchAnalysisInProgressView();
+                //get a handle to the stage
+                Stage stage = (Stage) startAnalysisButton.getScene().getWindow();
+                //close current window
+                stage.close();
+            }
+        });
     }
 
 
-    private void launchNeuroSkyView() {
+    private void launchAnalysisInProgressView() {
         try {
-            FXMLLoader loader = new FXMLLoader(getClass().getResource("../views/menuView.fxml"));
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("../views/analysisInProgressView.fxml"));
             Parent parent = (Parent) loader.load();
             Stage stage = new Stage();
             stage.setScene(new Scene(parent));
