d846f7b77767ef3da8bacca0467fffeb99017495
Luís Natividade <luis_natividade_@hotmail.com>
2016-06-03 02:39:28 +0100

    fix

--- poc/Patient/src/com/lgp5/patient/controllers/SettingsController.java
+++ poc/Patient/src/com/lgp5/patient/controllers/SettingsController.java
@@ -1,15 +1,16 @@
 package com.lgp5.patient.controllers;
 
 
-import javafx.fxml.FXML;
-import javafx.scene.control.Button;
-import javafx.scene.input.MouseEvent;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
-import javafx.stage.Stage;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.input.MouseEvent;
+import javafx.stage.Stage;
+
 import java.io.IOException;
 
 
