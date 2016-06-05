f07c0cbd4048e434ed5b3bf8236525379edebb55
João Monteiro <joaomiguelmonteiro@gmail.com>
2016-06-05 17:02:39 +0100

    Refactored project
    Some import issues still exist in MainModule.java

---
+++ BrainLight/Logic/src/Neurosky/src/interfaces/HeadSetDataInterface.java
@@ -0,0 +1,8 @@
+package Neurosky.src.interfaces;
+
+import java.util.HashMap;
+
+public interface HeadSetDataInterface {
+	public void onReceiveData(HashMap<String, HashMap<String,Object>> dataToSend);
+	public void onReceiveRawData(HashMap<String,Integer> rawData);
+}
